9b1a236c6f78177a0afc2f00752090c9bff472ea
Yahya Fallah <yahya.fallah.araghi@gmail.com>
2019-02-15 10:51:14 +0100

    s6d5e2 cocktail1...

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, :cocktail, :ingredient, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint = Ingredient.create(name: "mint leaves")
+sugar = Ingredient.create(name: "sugar can")
+
+cocktail1 = Cocktail.create(name: "Mojito")
+cocktail2 = Cocktail.create(name: "Cuba Libre")
+# cocktail3 = Cocktail.create(name: "Piña colada")
+
+Dose.create!(description: '6cl of rhum', cocktail: cocktail1, ingredient: lemon)
+Dose.create!(description: '2 spoon', cocktail: cocktail1, ingredient: sugar)
+Dose.create!(description: 'mint', cocktail: cocktail1, ingredient: mint)
+
+Dose.create!(description: '6cl of rhum', cocktail: cocktail2, ingredient: lemon)
+Dose.create!(description: 'ice', cocktail: cocktail2, ingredient: ice)
+Dose.create!(description: '2 spoon of sugar', cocktail: cocktail2, ingredient: sugar)
